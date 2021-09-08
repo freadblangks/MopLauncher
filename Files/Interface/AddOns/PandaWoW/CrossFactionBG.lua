d6d896a84b8cf1eaf8ba4ab33acf3ec284ee261f
DeadMouse <max180297@ya.ru>
2021-09-08 14:23:48 +0300

    button (Neff was here).

--- Files/Interface/AddOns/PandaWoW/CrossFactionBG.lua
+++ Files/Interface/AddOns/PandaWoW/CrossFactionBG.lua
@@ -150,6 +150,14 @@ soloFrame:SetScript("OnEvent", function(self, event, addonName)
                 if ConquestFrame.selectedButton.id == 3 and GetNumGroupMembers() == 0 then
                     ConquestFrame.JoinButton.tooltip = nil;
                     ConquestFrame.JoinButton:Enable();
+				elseif ConquestFrame.selectedButton.id == 4 then
+				    if GetNumGroupMembers() == 0 then
+					    ConquestFrame.RatedBG.TeamSizeText:SetText(GetLocale()=='ruRU' and 'Соло' or 'Solo');
+						ConquestFrame.JoinButton.tooltip = nil;
+                        ConquestFrame.JoinButton:Enable();
+					else
+					    ConquestFrame.RatedBG.TeamSizeText:SetText(CONQUEST_SIZE_STRINGS[ConquestFrame.RatedBG:GetID()]);
+					end
                 end
             end)
         elseif addonName == "Blizzard_InspectUI" then
